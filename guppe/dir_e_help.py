b5731e81b046e4ca6a4dc30bfab4f554516f121f
Juliano <juliano.co.lopes@gmail.com>
2020-06-02 15:55:10 -0300

    Aula dir e help

---
+++ guppe/dir_e_help.py
@@ -0,0 +1,14 @@
+"""
+Utilitários Python para te auxiliar na programação
+dir -> Apresenta todos os atribútos  e funções/métodos disponíveis para determinado tipo de dado ou variável.
+
+dir(tipo de dado/variável)
+
+help -> Apresenta a documentação/como utilizar os atributos/propriedades  e funções/métodos disponíveis para determinado tipo de dado ou variável.
+
+help(tipo de dado/variável.propriedade)
+
+# comando dir e help são melhores visualizados no console(terminal)
+"""
+
+print(dir('Geek'))
