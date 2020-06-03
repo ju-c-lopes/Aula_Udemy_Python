5ad893782ac74fcbe4a63d8909dd3a92b5dcebb8
Juliano <juliano.co.lopes@gmail.com>
2020-06-03 15:18:16 -0300

    Aula print, input, num_inteiros

---
+++ guppe/tipo_numerico.py
@@ -0,0 +1,32 @@
+
+# Tipo Numérico
+
+num = 1_000_000     # para facilitar nossa compreensão,
+                    # o Python permite que digitemos os milhares separados por underline,
+                    # e recebe o valor normalmente (veja com print)
+
+print(num)
+print(num - 1)
+
+# as operações também receberam certa facilidade para serem lidas e executadas
+# exemplo:
+
+# Soma
+num = 10
+num += 1
+print(f'\nSoma {num}\n')
+
+# Subtração
+num = 10
+num -= 1
+print(f'Subtração {num}\n')
+
+# Multiplicação
+num = 10
+num *= 2
+print(f'Multiplicação {num}\n')
+
+# Divisão
+num = 10
+num /= 2
+print(f'Divisão {num}')
