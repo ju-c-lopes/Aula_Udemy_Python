786695472648d26169128d3599123d730a0d4c65
Juliano <juliano.co.lopes@gmail.com>
2020-09-05 16:35:33 -0300

    Update

--- guppe/exercicios/exercicio_secao8_04.py
+++ guppe/exercicios/exercicio_secao8_04.py
@@ -14,7 +14,7 @@ n = int(n)
 
 
 def ver_quadrado_perfeito(num):
-    print(num)
+    
     if num == (sqrt(num) * sqrt(num)):
         return f'O número {num} é um quadrado perfeito'
     return f'O número {num} não é um quadrado perfeito'
